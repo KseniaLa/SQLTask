fd04b005732818e43eca5fd9b4a8ca6547ff5e14
KseniaLa <ksla1998@mail.ru>
2019-04-24 11:01:13 +0300

    add view for UnpaidBlogs

---
+++ Views/UnpaidBlogs.sql
@@ -0,0 +1,13 @@
+USE Task;   
+GO  
+CREATE VIEW dbo.UnpaidBlogs  
+AS  
+	SELECT Blogs.Id, Blogs.Name AS 'Blog Name', Users.Id AS 'UserId', Users.Name AS 'User Name', 
+	(SELECT COUNT(*) FROM Articles WHERE IsBlocked = 1 AND Articles.BlogId = Blogs.Id) AS 'Blocked Articles Count', 
+	(SELECT COUNT(*) FROM Articles WHERE IsBlocked = 0 AND Articles.BlogId = Blogs.Id) AS 'Opened Articles Count', 
+	COUNT(Articles.Id) AS 'Total Articles Count', AVG(CASE WHEN Articles.Rating > 0 THEN Articles.Rating ELSE NULL END) AS 'Blog Rating' FROM Blogs 
+	INNER JOIN Users ON Blogs.UserId = Users.Id
+	LEFT JOIN Articles ON Articles.BlogId = Blogs.Id
+	WHERE IsPaid = 0
+	GROUP BY Blogs.Id, Users.Id, Blogs.Name, Users.Name; 
+GO  
