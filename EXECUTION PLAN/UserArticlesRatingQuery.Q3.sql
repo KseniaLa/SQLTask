149dafe2b485df4ef981a5bef5db14f59eae583d
KseniaLa <ksla1998@mail.ru>
2019-04-24 23:44:23 +0300

    add Q3 optimization

--- EXECUTION PLAN/UserArticlesRatingQuery.Q3.sql
+++ EXECUTION PLAN/UserArticlesRatingQuery.Q3.sql
@@ -1,3 +1,4 @@
+--initial
 SELECT Users.Name, Users.Email, AVG(Rating) AS 'Avarage Rating' FROM Users
 	LEFT JOIN Articles
 	ON  Rating > 0 AND Articles.Id IN 
@@ -5,9 +6,10 @@ SELECT Users.Name, Users.Email, AVG(Rating) AS 'Avarage Rating' FROM Users
 		(SELECT Id FROM Blogs WHERE UserId = Users.Id)) 
 		GROUP BY Users.Name, Users.Email;
 
+-- optimized
 SELECT Users.Name, Users.Email, AVG(Rating) AS 'Avarage Rating' FROM Users
+	LEFT JOIN Blogs ON Blogs.UserId = Users.Id
 	LEFT JOIN Articles
-	ON  Rating > 0 AND Articles.Id IN 
-	(SELECT Articles.Id FROM Articles INNER JOIN Blogs ON BlogId = Blogs.Id WHERE UserId = Users.Id)
-		GROUP BY Users.Name, Users.Email;
+	ON  Rating > 0 AND Articles.BlogId = Blogs.Id
+	GROUP BY Users.Name, Users.Email;
 
