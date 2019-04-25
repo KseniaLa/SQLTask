59a4a34ea6f846ac27eb308fd2cd9f7f011529b7
KseniaLa <ksla1998@mail.ru>
2019-04-25 20:56:11 +0300

    add Q2 optimization

--- EXECUTION PLAN/UnpaidBlogsQuery.Q2.sql
+++ EXECUTION PLAN/UnpaidBlogsQuery.Q2.sql
@@ -1,6 +1,18 @@
+--initial
 SELECT Blogs.Name AS 'Blog Name', Users.Name AS 'User Name', 
 (SELECT COUNT(*) FROM Articles WHERE IsBlocked = 1 AND Articles.BlogId = Blogs.Id) AS 'Blocked Articles Count', 
 (SELECT COUNT(*) FROM Articles WHERE IsBlocked = 0 AND Articles.BlogId = Blogs.Id) AS 'Opened Articles Count', 
+COUNT(Articles.Id) AS 'Total Articles Count', AVG(CASE WHEN Articles.Rating > 0 THEN Articles.Rating ELSE NULL END) AS 'Blog Rating' FROM Blogs 
+INNER JOIN Users ON Blogs.UserId = Users.Id
+LEFT JOIN Articles ON Articles.BlogId = Blogs.Id
+WHERE IsPaid = 0
+GROUP BY Blogs.Name, Users.Name, Blogs.Id;
+
+
+--optimized
+SELECT Blogs.Name AS 'Blog Name', Users.Name AS 'User Name', 
+COUNT(CASE WHEN Articles.IsBlocked = 1 AND Articles.BlogId = Blogs.Id THEN Articles.Rating ELSE NULL END) AS 'Blocked Articles Count', 
+COUNT(CASE WHEN Articles.IsBlocked = 0 AND Articles.BlogId = Blogs.Id THEN Articles.Rating ELSE NULL END) AS 'Opened Articles Count', 
 COUNT(Articles.Id) AS 'Total Articles Count', AVG(CASE WHEN Articles.Rating > 0 THEN Articles.Rating ELSE NULL END) AS 'Blog Rating' FROM Blogs 
 INNER JOIN Users ON Blogs.UserId = Users.Id
 LEFT JOIN Articles ON Articles.BlogId = Blogs.Id
