84c1fda4eff3f9aca085967e657f67cdf1aaa072
KseniaLa <ksla1998@mail.ru>
2019-04-25 20:36:04 +0300

    add Q1 optimization

--- EXECUTION PLAN/ArticlesAfterDateQuery.Q1.sql
+++ EXECUTION PLAN/ArticlesAfterDateQuery.Q1.sql
@@ -1,5 +1,15 @@
+--initial
 SELECT Articles.Name AS 'Article Name', CONCAT(Blogs.Id, ' - ', Blogs.Name, ' - Created on ', Blogs.CreatedDate) AS 'Blog Info', 
 	CONCAT(Users.Id, ' - ', Users.Name, ' - ', Users.Email) AS 'User Info', COUNT(Comments.Id) AS 'Comments Count' FROM Articles 
+INNER JOIN Blogs ON Blogs.Id = BlogId
+INNER JOIN Users ON Blogs.UserId = Users.Id
+LEFT JOIN Comments ON Comments.ArticleId = Articles.Id
+WHERE Articles.CreatedDate > '2018-01-01'
+GROUP BY Articles.Name, Blogs.Name, Blogs.CreatedDate, Users.Name, Users.Id, Users.Email, Blogs.Id;
+
+--warning avoided, index to Articles.CreatedDate was added
+SELECT Articles.Name AS 'Article Name', Blogs.Id AS 'BlogId', Blogs.Name AS 'Blog Name', Blogs.CreatedDate AS 'Blog Created Date', 
+	CONCAT(Users.Name, ' - ', Users.Email) AS 'User Info', COUNT(Comments.Id) AS 'Comments Count' FROM Articles 
 INNER JOIN Blogs ON Blogs.Id = BlogId
 INNER JOIN Users ON Blogs.UserId = Users.Id
 LEFT JOIN Comments ON Comments.ArticleId = Articles.Id
